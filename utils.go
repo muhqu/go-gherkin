493c368be4afdb7a5d44e34229a5c42381c6cc3a
Mathias Leppich <mleppich@muhqu.de>
2016-04-11 17:13:32 +0200

    omit successive empty lines in descriptions

--- utils.go
+++ utils.go
@@ -17,7 +17,26 @@ func trimWSML(str string) string {
 	for i, line := range lines {
 		lines[i] = trimWS(line)
 	}
+	lines = omitSuccessiveEmptyLines(lines)
 	return trimWS(strings.Join(lines, "\n"))
+}
+
+func omitSuccessiveEmptyLines(lines []string) []string {
+	var out []string
+	var b1, b2 bool
+	for _, line := range lines {
+		if line != "" {
+			if b1 && !b2 {
+				out = append(out, "")
+			}
+			out = append(out, line)
+			b1 = true
+			b2 = true
+		} else {
+			b2 = false
+		}
+	}
+	return out
 }
 
 func trimNL(str string) string {
